a884d61b60092a5661a12d04fcadb0e54161f1d6
Meysam GanJi <p.g.meysam@gmail.com>
2019-05-26 14:58:59 +0430

    implement base data-mapper model

---
+++ Components/Databases/Model.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Espricho\Components\Databases;
+
+/**
+ * Class Model provides common a data mapper model functionality
+ *
+ * @package Espricho\Components\Databases
+ */
+class Model
+{
+
+}
