40d1b934056b5b8db2ee45a9ef5008cfe07f49dc
Meysam GanJi <p.g.meysam@gmail.com>
2019-05-26 17:00:22 +0430

    create base EntityRepository

---
+++ Components/Repositories/EntityRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Espricho\Components\Repositories;
+
+use Doctrine\ORM\EntityRepository as BaseEntityRepository;
+
+/**
+ * Class EntityRepository adds some functionality over Doctrine EntityRepository
+ *
+ * @package Espricho\Components\Repositories
+ */
+class EntityRepository extends BaseEntityRepository
+{
+
+}
