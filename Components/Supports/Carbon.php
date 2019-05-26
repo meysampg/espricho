0f30effbe2afd422c33dbe3449be76f34b580fae
Meysam GanJi <p.g.meysam@gmail.com>
2019-05-26 15:50:36 +0430

    install Carbon library

---
+++ Components/Supports/Carbon.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Espricho\Components\Supports;
+
+use Carbon\Carbon as BaseCarbon;
+
+/**
+ * Class Carbon provides some functionality over Carbon
+ *
+ * @package Espricho\Components\Supports
+ */
+class Carbon extends BaseCarbon
+{
+
+}
