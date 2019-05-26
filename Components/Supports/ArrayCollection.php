e01d6f82bb96f9463111723557cf77422b991475
Meysam GanJi <p.g.meysam@gmail.com>
2019-05-26 19:20:16 +0430

    create ArrayCollection class

---
+++ Components/Supports/ArrayCollection.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Espricho\Components\Supports;
+
+use Doctrine\Common\Collections\ArrayCollection as BaseArrayCollection;
+
+/**
+ * Class ArrayCollection add some functionality over Doctrine ArrayCollection
+ *
+ * @package Espricho\Components\Supports
+ */
+class ArrayCollection extends BaseArrayCollection
+{
+
+}
