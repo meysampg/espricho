d49e8a326363bd6b557616f39e1e91c2dc526fb3
Meysam GanJi <p.g.meysam@gmail.com>
2019-05-26 16:11:11 +0430

    add time default fields to model ({created, updated, deleted}_at)

--- Components/Databases/Model.php
+++ Components/Databases/Model.php
@@ -2,6 +2,9 @@
 
 namespace Espricho\Components\Databases;
 
+use Doctrine\ORM\Mapping as ORM;
+use Espricho\Components\Supports\Carbon;
+
 /**
  * Class Model provides common a data mapper model functionality
  *
@@ -9,5 +12,51 @@ namespace Espricho\Components\Databases;
  */
 class Model
 {
+    /**
+     * @var string
+     * @ORM\Column(type="datetime")
+     */
+    protected $created_at;
 
+    /**
+     * @var string
+     * @ORM\Column(type="datetime")
+     */
+    protected $updated_at;
+
+    /**
+     * @var string
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    protected $deleted_at;
+
+    public function getCreatedAt(): Carbon
+    {
+        return Carbon::createFromTimeString($this->created_at);
+    }
+
+    public function setCreatedAt(string $dt)
+    {
+        $this->created_at = $dt;
+    }
+
+    public function getUpdatedAt(): Carbon
+    {
+        return Carbon::createFromTimeString($this->updated_at);
+    }
+
+    public function setUpdatedAt(string $dt)
+    {
+        $this->updated_at = $dt;
+    }
+
+    public function getDeletedAt(): Carbon
+    {
+        return Carbon::createFromTimeString($this->deleted_at);
+    }
+
+    public function setDeletedAt(string $dt)
+    {
+        $this->deleted_at = $dt;
+    }
 }
