9c05954abe7b1bbaafdd09df53234e27ba25abd3
Meysam GanJi <p.g.meysam@gmail.com>
2019-05-28 22:38:39 +0430

    implement support of adding middleware to the container

--- Components/Application/Application.php
+++ Components/Application/Application.php
@@ -4,14 +4,18 @@ namespace Espricho\Components\Application;
 
 use Exception;
 use Symfony\Component\Debug\Debug;
+use Espricho\Components\Contracts\Middleware;
 use Espricho\Components\Configs\ConfigCollection;
 use Espricho\Components\Contracts\KernelInterface;
 use Espricho\Components\Contracts\Authenticatable;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Espricho\Components\Configs\Traits\ConfigCommonMethodsTrait;
+use Espricho\Components\Http\Exceptions\InvalidMiddlewareClassException;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
+use function is_a;
 use function sprintf;
+use function is_string;
 
 /**
  * Class Application provides the core application class
@@ -35,6 +39,13 @@ class Application extends ContainerBuilder
      * @var Authenticatable
      */
     protected $authenticated_user;
+
+    /**
+     * Store a list of middlewares
+     *
+     * @var array
+     */
+    protected $middlewares = [];
 
     /**
      * Application constructor.
@@ -118,6 +129,52 @@ class Application extends ContainerBuilder
     public function getUser(): Authenticatable
     {
         return $this->authenticated_user;
+    }
+
+    /**
+     * Return all middlewares
+     * TODO: implement a mechanism for separating route middlewares and global middlewares
+     *
+     * @return array
+     */
+    public function getMiddlewares(): array
+    {
+        return $this->middlewares;
+    }
+
+    /**
+     * Get a requested middleware
+     *
+     * @param string $key
+     *
+     * @return Middleware|null
+     */
+    public function getMiddleware(string $key): ?Middleware
+    {
+        if (!isset($this->middlewares[$key])) {
+            return null;
+        }
+
+        $middleware = $this->middlewares[$key];
+
+        return (is_string($middleware) ? new $middleware : $middleware);
+    }
+
+    /**
+     * Set a middleware
+     *
+     * @param string            $alias
+     * @param Middleware|string $middleware
+     *
+     * @throws InvalidMiddlewareClassException
+     */
+    public function setMiddleware(string $alias, $middleware): void
+    {
+        if (!is_a($middleware, Middleware::class)) {
+            throw new InvalidMiddlewareClassException(sprintf("%s is not a valid middleware class. Make sure it implements %s.", $middleware, Middleware::class));
+        }
+
+        $this->middlewares[$alias] = $middleware;
     }
 
     /**
