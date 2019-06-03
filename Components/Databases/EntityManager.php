5f8adf09e10664a0c7eb35031e759378216e3bb7
Meysam GanJi <p.g.meysam@gmail.com>
2019-06-03 20:18:03 +0430

    add findOrCreate method

--- Components/Databases/EntityManager.php
+++ Components/Databases/EntityManager.php
@@ -6,5 +6,30 @@ use Doctrine\ORM\EntityManager as BaseEntityManager;
 
 class EntityManager extends BaseEntityManager
 {
+    /**
+     * Find or create a model and populate it with given data. If it fail, return
+     * an empty instance.
+     *
+     * @param string $model
+     * @param array  $data
+     *
+     * @return Model
+     */
+    public static function findOrCreate(string $model, array $data): Model
+    {
+        if (isset($data['id'])) {
+            $fetched = em()->getRepository($model)->findOneBy(['id' => $data['id']]);
+            if ($fetched) {
+                $model = $fetched;
+            } else {
+                $model = new $model;
+            }
+        } else {
+            $model = new $model();
+        }
 
+        $model->load($data);
+
+        return $model;
+    }
 }
