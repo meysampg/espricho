0d7aba74abd573eafdef9e3b8eff314393539dfa
Meysam GanJi <p.g.meysam@gmail.com>
2019-06-08 22:00:09 +0430

    add request to route resolve event

--- Components/Routes/Events/RouteResolvedEvent.php
+++ Components/Routes/Events/RouteResolvedEvent.php
@@ -4,14 +4,18 @@ namespace Espricho\Components\Routes\Events;
 
 use Symfony\Component\Routing\Route;
 use Symfony\Component\EventDispatcher\Event;
+use Symfony\Component\HttpFoundation\Request;
 
 class RouteResolvedEvent extends Event
 {
     protected $route;
 
-    public function __construct(Route $route)
+    protected $request;
+
+    public function __construct(Route $route, Request $request)
     {
-        $this->route = $route;
+        $this->route   = $route;
+        $this->request = $request;
     }
 
     /**
@@ -28,5 +32,21 @@ class RouteResolvedEvent extends Event
     public function setRoute(Route $route): void
     {
         $this->route = $route;
+    }
+
+    /**
+     * @return Request
+     */
+    public function getRequest(): Request
+    {
+        return $this->request;
+    }
+
+    /**
+     * @param Request $request
+     */
+    public function setRequest(Request $request): void
+    {
+        $this->request = $request;
     }
 }
